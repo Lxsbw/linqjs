ce45112e6935fd7773289c10a7c2b9a8c58a3707
zhixiong <lxsbw@outlook.com>
2023-07-31 20:44:56 +0800

    Array.prototype

--- test/arr/arr_init.js
+++ test/arr/arr_init.js
@@ -1,17 +1,254 @@
 (function () {
-  Array.prototype.where = function (predicate) {
-    return this.filter(predicate);
+  var Linq = {};
+
+  /**
+   * Adds an object to the end of the List<T>.
+   */
+  Linq.add = function (element) {
+    this.push(element);
   };
 
-  Array.prototype.select = function (selector) {
-    return this.map(selector);
+  /**
+   * Adds the elements of the specified collection to the end of the List<T>.
+   */
+  Linq.addRange = function (elements) {
+    var _a;
+    (_a = this).push.apply(_a, elements);
   };
 
-  Array.prototype.aggregate = function (accumulator, initialValue) {
+  /**
+   * Applies an accumulator function over a sequence.
+   */
+  Linq.aggregate = function (accumulator, initialValue) {
     return this.reduce(accumulator, initialValue);
   };
 
-  Array.prototype.sum = function (transform) {
+  /**
+   * Determines whether all elements of a sequence satisfy a condition.
+   */
+  Linq.all = function (predicate) {
+    return this.every(predicate);
+  };
+
+  /**
+   * Determines whether a sequence contains any elements.
+   */
+  Linq.any = function (predicate) {
+    return predicate ? this.some(predicate) : this.length > 0;
+  };
+
+  /**
+   * Computes the average of a sequence of number values that are obtained by invoking
+   * a transform function on each element of the input sequence.
+   */
+  Linq.average = function (transform) {
+    return tools.calcNumDiv(this.sum(transform), this.count());
+  };
+
+  /**
+   * Returns the number of elements in a sequence.
+   */
+  Linq.count = function (predicate) {
+    return predicate ? this.where(predicate).count() : this.length;
+  };
+
+  /**
+   * Returns distinct elements from a sequence by using the default equality comparer to compare values.
+   */
+  Linq.distinct = function () {
+    return this.where(function (value, index, iter) {
+      return (
+        (tools.isObject(value)
+          ? iter.findIndex(function (obj) {
+              return tools.equal(obj, value);
+            })
+          : iter.indexOf(value)) === index
+      );
+    });
+  };
+
+  /**
+   * Returns distinct elements from a sequence according to specified key selector.
+   */
+  Linq.distinctBy = function (keySelector) {
+    var groups = this.groupBy(keySelector);
+
+    const func = function (res, key) {
+      const curr = groups.firstOrDefault(x => tools.equal(x.key, key));
+      res.add(curr.elements[0]);
+      return res;
+    };
+
+    return groups.select(x => x.key).reduce(func, []);
+  };
+
+  /**
+   * Returns distinct elements from a sequence by using the default equality comparer to compare values and this.select method.
+   */
+  Linq.distinctMap = function (predicate) {
+    return predicate ? this.select(predicate).distinct() : this.distinct();
+  };
+
+  /**
+   * Returns the element at a specified index in a sequence.
+   */
+  Linq.elementAt = function (index) {
+    if (index < this.count() && index >= 0) {
+      return this[index];
+    } else {
+      throw new Error('ArgumentOutOfRangeException: index is less than 0 or greater than or equal to the number of elements in source.');
+    }
+  };
+
+  /**
+   * Returns the first element of a sequence.
+   */
+  Linq.first = function (predicate) {
+    if (this.count()) {
+      return predicate ? this.where(predicate).first() : this[0];
+    } else {
+      throw new Error('InvalidOperationException: The source sequence is empty.');
+    }
+  };
+
+  /**
+   * Returns the first element of a sequence, or a default value if the sequence contains no elements.
+   */
+  Linq.firstOrDefault = function (predicate) {
+    return this.count(predicate) ? this.first(predicate) : undefined;
+  };
+
+  /**
+   * Groups the elements of a sequence according to a specified key selector function.
+   */
+  Linq.groupBy = function (grouper, mapper) {
+    if (mapper === void 0) {
+      mapper = function (val) {
+        return val;
+      };
+    }
+    var initialValue = [];
+
+    const func = function (ac, v) {
+      var key = grouper(v);
+      var existingGroup = ac.firstOrDefault(x => tools.equal(x.key, key));
+      var mappedValue = mapper(v);
+
+      if (existingGroup) {
+        existingGroup.elements.push(mappedValue);
+        existingGroup.count++;
+      } else {
+        let existingMap = {
+          key: key,
+          count: 1,
+          elements: [mappedValue]
+        };
+        ac.push(existingMap);
+      }
+      return ac;
+    };
+
+    return this.aggregate(func, initialValue);
+  };
+
+  /**
+   * Correlates the elements of two sequences based on matching keys. The default equality comparer is used to compare keys.
+   */
+  Linq.join = function (list, key1, key2, result) {
+    const selectmany = selector => {
+      return this.aggregate((ac, _, i) => {
+        return ac.addRange(this.select(selector).elementAt(i)), ac;
+      }, []);
+    };
+
+    return selectmany(function (x) {
+      return list
+        .where(function (y) {
+          return key2(y) === key1(x);
+        })
+        .select(function (z) {
+          return result(x, z);
+        });
+    });
+  };
+
+  /**
+   * Returns the maximum value in a generic sequence.
+   */
+  Linq.max = function (selector) {
+    var id = function (x) {
+      return x;
+    };
+    return Math.max.apply(Math, this.map(selector || id));
+  };
+
+  /**
+   * Returns the minimum value in a generic sequence.
+   */
+  Linq.min = function (selector) {
+    var id = function (x) {
+      return x;
+    };
+    return Math.min.apply(Math, this.map(selector || id));
+  };
+
+  /**
+   * Removes the first occurrence of a specific object from the List<T>.
+   */
+  Linq.remove = function (element) {
+    return this.indexOf(element) !== -1 ? (this.removeAt(this.indexOf(element)), true) : false;
+  };
+
+  /**
+   * Removes all the elements that match the conditions defined by the specified predicate.
+   */
+  Linq.removeAll = function (predicate) {
+    return this.where(tools.negate(predicate));
+  };
+
+  /**
+   * Removes the element at the specified index of the List<T>.
+   */
+  Linq.removeAt = function (index) {
+    this.splice(index, 1);
+  };
+
+  /**
+   * Projects each element of a sequence into a new form.
+   */
+  Linq.select = function (selector) {
+    return this.map(selector);
+  };
+
+  /**
+   * Projects each element of a sequence to a List<any> and flattens the resulting sequences into one sequence.
+   */
+  Linq.selectMany = function (selector) {
+    var _this = this;
+    return this.aggregate(function (ac, _, i) {
+      return ac.addRange(_this.select(selector).elementAt(i)), ac;
+    }, []);
+  };
+
+  /**
+   * Bypasses a specified number of elements in a sequence and then returns the remaining elements.
+   */
+  Linq.skip = function (amount) {
+    return this.slice(Math.max(0, amount));
+  };
+
+  /**
+   * Omit the last specified number of elements in a sequence and then returns the remaining elements.
+   */
+  Linq.skipLast = function (amount) {
+    return this.slice(0, -Math.max(0, amount));
+  };
+
+  /**
+   * Computes the sum of the sequence of number values that are obtained by invoking
+   * a transform function on each element of the input sequence.
+   */
+  Linq.sum = function (transform) {
     return transform
       ? this.select(transform).sum()
       : this.aggregate(function (ac, v) {
@@ -19,13 +256,50 @@
         }, 0);
   };
 
-  Array.prototype.average = function (transform) {
-    return tools.calcNumDiv(this.sum(transform), this.count());
+  /**
+   * Returns a specified number of contiguous elements from the start of a sequence.
+   */
+  Linq.take = function (amount) {
+    return this.slice(0, Math.max(0, amount));
   };
 
-  Array.prototype.count = function (predicate) {
-    return predicate ? this.where(predicate).count() : this.length;
+  /**
+   * Returns a specified number of contiguous elements from the end of a sequence.
+   */
+  Linq.takeLast = function (amount) {
+    return this.slice(-Math.max(0, amount));
   };
+
+  /**
+   * Copies the elements of the List<T> to a new array.
+   */
+  Linq.toArray = function () {
+    return this;
+  };
+
+  /**
+   * Creates a Dictionary<TKey, TValue> from a List<T> according to a specified key selector function.
+   */
+  Linq.toDictionary = function (key, value) {
+    var _this = this;
+    return this.aggregate(function (dicc, v, i) {
+      dicc[_this.select(key).elementAt(i).toString()] = value ? _this.select(value).elementAt(i) : v;
+      dicc.add({
+        Key: _this.select(key).elementAt(i),
+        Value: value ? _this.select(value).elementAt(i) : v
+      });
+      return dicc;
+    }, []);
+  };
+
+  /**
+   * Filters a sequence of values based on a predicate.
+   */
+  Linq.where = function (predicate) {
+    return this.filter(predicate);
+  };
+
+  Object.assign(Array.prototype, Linq);
 })();
 
 /**
