94544c1f1e7d19dc0fd09b330530ed8f1c38ce40
zhixiong <lxsbw@outlook.com>
2025-02-16 14:57:27 +0800

    unit test example

--- test/example/groupby.js
+++ test/example/groupby.js
@@ -6,7 +6,11 @@ Linq = require('../../src/linq');
 data = [
   { id: 1, name: 'one', category: 'fruits', countries: ['lxsbw', 'xliecz'] },
   { id: 1, name: 'one', category: 'fruits', countries: ['Italy', 'Austria'] },
-  { id: 2, name: 'two', category: 'vegetables', countries: ['Italy', 'Germany'] }
+  { id: 2, name: 'two', category: 'vegetables', countries: ['Italy', 'Germany'] },
+  { id: 2, name: 'two', category: null, countries: ['Italy', 'Germany'] },
+  { id: 2, name: 'two', category: undefined, countries: ['Italy', 'Germany'] },
+  { id: 2, name: 'two', category: '', countries: ['Italy', 'Germany'] },
+  { id: 2, name: 'two', countries: ['Italy', 'Germany'] }
   // { id: 3, name: 'three', category: 'vegetables', countries: ['Germany'] },
   // { id: 4, name: 'four', category: 'fruits', countries: ['Japan'] },
   // { id: 5, name: 'five', category: 'fruits', countries: ['Japan', 'Italy'] }
@@ -23,7 +27,7 @@ result = new Linq(data).groupBy(el => el.category);
 // };
 // result = new Linq(data).groupBy((el) => el.category, func);
 
-result = new Linq(data).toLookup(el => el.category);
+// result = new Linq(data).toLookup(el => el.category);
 
 // result.forEach(x => console.log(x.key.toString(), x.count));
 
