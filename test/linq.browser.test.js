e840e676d6d972fde10127d58715a8bc956cb30f
zhixiong <lxsbw@outlook.com>
2025-04-30 16:54:42 +0800

    update

--- test/linq.browser.test.js
+++ test/linq.browser.test.js
@@ -4,19 +4,19 @@
 
 describe('Linq Browser Environment', () => {
   beforeAll(() => {
-    require('../src/linq'); // 直接引入模块
+    require('../src/linq');
   });
 
-  test('Linq 应挂载到 window', () => {
-    // 由于已经通过 require 执行过代码，这里可以直接检查 window 对象
+  test('Linq in window', () => {
+    // 已 require 执行过代码，直接检查 window 对象
     expect(Linq).toBeDefined();
     expect(window.Linq).toBeDefined();
     expect(new window.Linq()).toBeInstanceOf(window.Linq);
     expect(new Linq()).toBeInstanceOf(window.Linq);
   });
 
-  test('add 方法添加元素', () => {
-    const linq = new Linq([1]); // 使用直接引入的 Linq 类
+  test('Add', () => {
+    const linq = new Linq([1]);
     linq.add(2);
     expect(linq.toArray()).toEqual([1, 2]);
   });
