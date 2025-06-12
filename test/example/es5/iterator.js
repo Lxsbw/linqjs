958afce9e923fbd3bab2d4bacc66cea1a68dca70
zhixiong <lxsbw@126.com>
2025-06-12 10:53:36 +0800

    update test file

---
+++ test/example/es5/iterator.js
@@ -0,0 +1,37 @@
+var Linq = require('../../../src/es5/linq');
+
+const pets = new Linq([
+  { Age: 10, Name: 'Barley' },
+  { Age: 4, Name: 'Boots' },
+  { Age: 6, Name: 'Bissy' },
+]);
+
+console.log('=============================== for...of ========================');
+for (const pet of pets) {
+  console.log('pet:', pet);
+}
+
+console.log();
+console.log('=============================== [...pets] ========================');
+const petsCopy = [...pets];
+for (const pet of petsCopy) {
+  console.log('petsCopy:', pet);
+}
+
+console.log();
+console.log('=============================== Array.from() ========================');
+const petsFrom = Array.from(pets);
+for (const pet of petsFrom) {
+  console.log('petsFrom:', pet);
+}
+
+console.log();
+console.log('=============================== new Set() ========================');
+const petsSet = new Set(pets);
+console.log('petsSet:', petsSet);
+
+
+console.log();
+const petsss = new Linq([]);
+console.log(petsss.toString() === '[object Linq]');
+console.log(`${petsss}` === '[object Linq]');
---
+++ test/example/es5/orderByDescending.js
@@ -0,0 +1,28 @@
+var Linq, orderedParameters, parameters;
+Linq = require('../../../src/es5/linq');
+
+parameters = [
+  { ID: 0, Name: '正一郎' },
+  { ID: 3, Name: '清次郎' },
+  { ID: 2, Name: '誠三郎' },
+  { ID: 5, Name: '征史郎' },
+];
+
+// console.log('parameters:', parameters);
+orderedParameters = new Linq(parameters).orderByDescending(x => x.ID).toArray();
+// console.log('global:', global.structuredClone === structuredClone);
+// console.log('globalnull:', structuredClone(null));
+// console.log('global123:', structuredClone(123));
+
+console.log('parameters:', parameters);
+console.log('orderedParameters:', orderedParameters);
+
+const parametersNull = [
+  { ID: null, Name: '正一郎' },
+  { ID: 3, Name: '清次郎' },
+  { ID: undefined, Name: '誠三郎' },
+  { ID: 5, Name: '征史郎' },
+];
+
+console.log('Empty1:', new Linq(parametersNull).orderBy(x => x.ID).toArray());
+console.log('Empty2:', new Linq(parametersNull).orderByDescending(x => x.ID).toArray());
