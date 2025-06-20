2266579aedac32d83cd3f1f768064ed03288634c
zhixiong <lxsbw@126.com>
2025-06-20 15:54:49 +0800

    独立测试套件

--- test/linq.browser.test.js
+++ test/linq.browser.test.js
@@ -1,11 +1,11 @@
 /**
  * @jest-environment jsdom
  */
-const testLinqImplementation = (implementationPath, implementationName) => {
-  describe(`Linq Browser Environment (${implementationName})`, () => {
+const testLinqImple = (implePath, impleName) => {
+  describe(`Linq Browser Environment (${impleName})`, () => {
     beforeAll(() => {
       jest.resetModules();
-      require(implementationPath);
+      require(implePath);
     });
 
     test('Linq in window', () => {
@@ -23,26 +23,8 @@ const testLinqImplementation = (implementationPath, implementationName) => {
   });
 };
 
-testLinqImplementation('../src/linq', 'ESNext');
-testLinqImplementation('../src/es5/linq', 'ES5');
-
-// describe('Linq Browser Environment', () => {
-//   beforeAll(() => {
-//     require('../src/linq');
-//     // require('../src/es5/linq');
-//   });
-
-//   test('Linq in window', () => {
-//     // 已 require 执行过代码，直接检查 window 对象
-//     expect(Linq).toBeDefined();
-//     expect(window.Linq).toBeDefined();
-//     expect(new window.Linq()).toBeInstanceOf(window.Linq);
-//     expect(new Linq()).toBeInstanceOf(window.Linq);
-//   });
-
-//   test('Add', () => {
-//     const linq = new Linq([1]);
-//     linq.add(2);
-//     expect(linq.toArray()).toEqual([1, 2]);
-//   });
-// });
+testLinqImple('../src/linq', 'ESNext');
+testLinqImple('../src/es5/linq', 'ES5');
+// testLinqImple('../build/linq.min.js', 'ESMini');
+// testLinqImple('../build/coffee_to_linq.js', 'ESCoffee');
+// testLinqImple('../build/linq.es5.min.js', 'ES5Mini');
